c10c18abf6682a39fce30fe77276cd9159643cd5
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-07-18 22:11:03 +0300

    Remove unused chat compatibility shims

--- app/chat/ChatAndroidPushActionsFix.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-// Временная совместимая заглушка; рабочая логика чата находится в ChatProvider.
-export default function ChatAndroidPushActionsFix() {
-  return null;
-}
--- app/chat/ChatCrossDeviceMessageFix.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ChatCrossDeviceMessageFix() {
-  return null;
-}
