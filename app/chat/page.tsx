308910b3442e331f5ee37888e2600cd1b740b432
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-07-18 17:58:12 +0300

    Remove global localStorage quota monkey patch

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import ChatOperationNotice from "./ChatOperationNotice";
 import ChatOutgoingOrderFix from "./ChatOutgoingOrderFix";
-import ChatOutboxQuotaGuard from "./ChatOutboxQuotaGuard";
 import ChatProvider from "./ChatProvider";
 import ChatScreen from "./ChatScreen";
 import ChatTopicResubscribe from "./ChatTopicResubscribe";
@@ -10,7 +9,6 @@ import ChatViewportFix from "./ChatViewportFix";
 export default function ChatPage() {
   return (
     <ChatProvider>
-      <ChatOutboxQuotaGuard />
       <ChatScreen />
       <ChatOperationNotice />
       <ChatOutgoingOrderFix />
