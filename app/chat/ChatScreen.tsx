99d725a803c1ce622c6d25c14008d7a19b186796
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-07-19 17:01:44 +0300

    Show chat connection state

--- app/chat/ChatScreen.tsx
+++ app/chat/ChatScreen.tsx
@@ -2,6 +2,7 @@
 
 import ChatActions from "./ChatActions";
 import ChatComposer from "./ChatComposer";
+import ChatConnectionStatus from "./ChatConnectionStatus";
 import ChatHeader from "./ChatHeader";
 import ChatMessageList from "./ChatMessageList";
 import { useChatController } from "./useChatController";
@@ -12,6 +13,7 @@ export default function ChatScreen() {
   return (
     <main data-hm51-chat-main="true" className="flex h-[100dvh] min-h-[100dvh] flex-col overflow-hidden bg-[#121715] text-white">
       <ChatHeader chat={chat} />
+      <ChatConnectionStatus chat={chat} />
       <ChatMessageList chat={chat} />
       <ChatComposer chat={chat} />
       <ChatActions chat={chat} />
