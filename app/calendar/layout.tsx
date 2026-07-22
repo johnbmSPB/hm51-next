c8c57a2940af2e7234314440d91ee3f90e15ae77
johnbmSPB <emelyantsevalmaz@gmail.com>
2026-07-22 20:17:34 +0300

    Connect calendar directly to global data revisions

--- app/calendar/layout.tsx
+++ app/calendar/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
-import DataRevisionBoundary from "../components/DataRevisionBoundary";
 
 export default function CalendarLayout({ children }: { children: ReactNode }) {
-  return <DataRevisionBoundary scope="calendar">{children}</DataRevisionBoundary>;
+  return children;
 }
