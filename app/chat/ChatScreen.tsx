532d7f768e010081d9193e4f493c6ed5624c8ed5
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-08-24 21:37:21 +0300

    Mount unread chat controller

--- app/chat/ChatScreen.tsx
+++ app/chat/ChatScreen.tsx
@@ -5,6 +5,7 @@ import ChatComposer from "./ChatComposer";
 import ChatConnectionStatus from "./ChatConnectionStatus";
 import ChatHeader from "./ChatHeader";
 import ChatMessageList from "./ChatMessageList";
+import ChatUnreadController from "./ChatUnreadController";
 import { useChatController } from "./useChatController";
 
 export default function ChatScreen() {
@@ -12,6 +13,7 @@ export default function ChatScreen() {
 
   return (
     <main data-hm51-chat-main="true" className="flex h-[100dvh] min-h-[100dvh] flex-col overflow-hidden bg-[#121715] text-white">
+      <ChatUnreadController />
       <ChatHeader chat={chat} />
       <ChatConnectionStatus chat={chat} />
       <ChatMessageList chat={chat} />
