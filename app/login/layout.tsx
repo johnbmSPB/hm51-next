f36fb391d929894c877d02910e51d135cab6fa5b
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-08-25 15:49:31 +0300

    Wire password restore dialog into login screen

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from "react";
+import PasswordRestoreDialog from "./PasswordRestoreDialog";
 import "./profile-selector-center.css";
 
 export default function LoginLayout({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <>
+      {children}
+      <PasswordRestoreDialog />
+    </>
+  );
 }
