016efe85b7ef5ac5c0cb6e463e8df4a8c222d987
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-08-25 13:49:21 +0300

    Move chat messages closer to composer

--- app/chat/ChatScreen.tsx
+++ app/chat/ChatScreen.tsx
@@ -13,6 +13,16 @@ export default function ChatScreen() {
 
   return (
     <main data-hm51-chat-main="true" className="flex h-[100dvh] min-h-[100dvh] flex-col overflow-hidden bg-[#121715] text-white">
+      <style>{`
+        [data-hm51-chat-messages="true"] {
+          padding-bottom: 4px !important;
+        }
+
+        [data-hm51-chat-messages="true"] > div:has(button) {
+          min-height: 100%;
+          justify-content: flex-end;
+        }
+      `}</style>
       <ChatUnreadController />
       <ChatHeader chat={chat} />
       <ChatConnectionStatus chat={chat} />
