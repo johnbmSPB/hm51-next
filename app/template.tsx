1bc86e57039ed84e44c49be7e2429b04696b84f7
Evgeniy Emelyantsev <john_bm@mail.ru>
2026-08-25 16:59:43 +0300

    Show password change in player profile

--- app/template.tsx
+++ app/template.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import GlobalChatUnreadBadge from "./components/GlobalChatUnreadBadge";
+import ProfileChangePassword from "./components/ProfileChangePassword";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <GlobalChatUnreadBadge />
+      <ProfileChangePassword />
       {children}
     </>
   );
